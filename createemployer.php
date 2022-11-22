4778592a64c4f15e38adc270c108b88791cf151d
Fahim Islam <tjfahim1997@gmail.com>
2022-11-22 10:31:58 +0600

    create employer done

---
+++ createemployer.php
@@ -0,0 +1,114 @@
+<?php 
+include('include/connection.php');
+
+if(isset($_POST['create'])){
+    $companyname = $_POST['cname'];
+    $username = $_POST['uname'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+    $error=array();
+    $sql_u = "SELECT * FROM users WHERE username='$username'";
+
+
+    if($password != $cpassword){
+        $error['e']= "Password Does not match";
+    }else if($username){
+        $query = "SELECT  username FROM employer WHERE username ='$username'";
+        $result = mysqli_query($con,$query);
+        if (mysqli_num_rows($result) > 0) {
+            $error['e'] = "Sorry... username already taken"; 	
+        }
+    }
+
+   
+
+    if(count($error)==0){
+    $query="INSERT INTO employer(companyname,username,email,phone,address,password,date_reg,status) VALUES ('$companyname','$username','$email','$phone','$address','$password',NOW(),'deactive')";
+    $result=mysqli_query($con,$query);
+    if($result){
+        echo"<script>alert('You Have Successfully Register')</script>";
+        header("location:employerlogin.php");
+
+    }else{
+        echo"<script>alert('Failed')</script>";
+    }
+
+    }
+ 
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create an account as employer</title>
+</head>
+    <?php 
+        include('include/header.php')
+    ?>
+    
+    <div class="container">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6 jumbotron">
+                    <h4 class="text-center">Employer Registration Form</h4>
+                    <form action="" method="post" class="py-2" enctype="multipart/form-data">
+                    <?php
+                            if(isset($error['e'])){
+                                $sh=$error['e'];
+                                $show="<h4 class='alert alert-danger'>$sh</h4>";
+                                echo $show;
+                                
+                            }
+                        
+                            ?>
+                   
+                        <div class="form-group">
+                            <label for="fname">Company Name</label>
+                            <input type="text" name="cname" class="form-control" autocomplete="off" placeholder="Enter First Name" value="<?php if(isset($_POST['fname'])){echo $_POST['fname'];}?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="uname">Username</label>
+                            <input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username" value="<?php if(isset($_POST['uname'])){echo $_POST['uname'];}?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" class="form-control" autocomplete="off" placeholder="Enter Email" value="<?php if(isset($_POST['email'])){echo $_POST['email'];}?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone Number</label>
+                            <input type="number" name="phone" class="form-control" autocomplete="off" placeholder="Enter Phone Number" value="<?php if(isset($_POST['phone'])){echo $_POST['phone'];}?>" required>
+                        </div>
+                     
+                        <div class="form-group">
+                            <label for="phone">Address</label>
+                            <input type="number" name="address" class="form-control" autocomplete="off" placeholder="Enter Phone Number" value="<?php if(isset($_POST['phone'])){echo $_POST['address'];}?>">
+                        </div>
+                       
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" autocomplete="off" placeholder="Enter Password" value="<?php if(isset($_POST['password'])){echo $_POST['password'];}?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="cpassword">Confirm Password</label>
+                            <input type="password" name="cpassword" class="form-control" placeholder="Enter Password" value="<?php if(isset($_POST['cpassword'])){echo $_POST['cpassword'];}?>" required>
+                        </div>
+                       
+                        <input type="submit" name="create" class="btn btn-success">
+                        <div>Already have an accout? <a href="employerlogin.php">Login</a></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ employerlogin.php
@@ -0,0 +1,89 @@
+<?php 
+include('include/connection.php');
+
+if(isset($_POST['login'])){
+    $username = $_POST['uname'];
+    $password = $_POST['password'];
+
+    $error=array();
+
+    if(empty($username)){
+        $error['employer']=['Enter Username'];
+    }else if(empty($password)){
+        $error['employer']=['Enter Password'];
+    }
+   
+    if(count($error)==0){
+        $query="select * from employer where username='$username' and password='$password'";
+        $result=mysqli_query($con,$query);
+        if(mysqli_num_rows($result)==1){
+            
+            echo"<script>alert('You are login as employer')</script>";
+            session_start();
+            $_SESSION['employer']=$username;
+            header("location:employer/index.php");
+            exit();
+
+        }else{
+           
+            echo"<script>alert('Invalid Username or Password')</script>";
+        }
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employer login page</title>
+</head>
+<body style="background-image:url(img/New-Project-64.jpg); background-repeat: no-repeat; background-size:cover ;"></body>
+    <?php 
+        include('include/header.php')
+    ?>
+    <div style="margin-top: 30px;"></div>
+    
+    <div class="container">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6 ">
+                    <form action="" method="post" class="py-2">
+                        <div class="">
+                        <h2 class="text-success">Login As Employer</h2>
+                            <?php
+                            if(isset($error['employer'])){
+                                $sh=$error['employer'];
+                                $show="<h4 class='alert alert-danger'>$sh[0]</h4>";
+                                echo $show;
+                                
+                            }else if(isset($error['employer'])){
+                                $show=$error['employer'];
+                                echo $show;
+                            } 
+                           
+                        
+                            ?>
+                        </div>
+                        <div class="form-group">
+                            <label for="uname">Username</label>
+                            <input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" placeholder="Enter Password">
+                        </div>
+                        <input type="submit" name="login" class="btn btn-success">
+                        <div class="mt-2">Dont have an account? <a href="createemployer.php" class="text-primary mt-2">Create Account</a></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
